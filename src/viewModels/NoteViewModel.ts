7c6661f8073f4a8689a0c3b4c3f42ac2307532cf
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-11-25 21:59:07 +0600

    create business logic in viewModel

---
+++ src/viewModels/NoteViewModel.ts
@@ -0,0 +1,16 @@
+import { Note } from "../models/Note";
+
+
+export class NoteViewModel {
+    private notes: Note[] = []
+
+    addNewNote(text: string): void {
+        const newNote: Note = {
+            id: Date.now().toString(),
+            text: text,
+            createdAt: new Date()
+        }
+        this.notes = [newNote, ...this.notes]
+        // this.notes.push(newNote)
+    }
+}
