9fc7e1f6f800fc34887129ee9b268adb94640ee9
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-11-25 22:17:19 +0600

    create noteItem components

---
+++ src/views/components/NoteItem.tsx
@@ -0,0 +1,48 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native"
+import { Note } from "../../models/Note"
+
+
+interface NoteItemProps {
+    note: Note,
+    onDelete: (id: string) => void
+}
+
+const NoteItem = ({ note, onDelete }: NoteItemProps) => {
+    return (
+        <TouchableOpacity style={styles.noteItem} onPress={() => onDelete(note.id)}>
+            <View>
+                <Text style={styles.noteText}>{note.text}</Text>
+                <Text style={styles.noteDate}>{note.createdAt.toLocaleString()}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    noteItem: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 10,
+        marginBottom: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    noteText: {
+        fontSize: 16,
+        fontWeight: '500',
+    },
+    noteDate: {
+        fontSize: 12,
+        color: '#666',
+        marginTop: 4
+    }
+})
